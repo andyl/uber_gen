a70b3a4a0d56b0c381e35b960ed99ee19ae80e69
andyl <andy@r210.com>
2019-12-06 14:20:35 -0800

    Add ability to read config file

--- lib/atree/config.ex
+++ lib/atree/config.ex
@@ -5,6 +5,7 @@ defmodule Atree.Config do
       "~/src/uber_gen",
       "~/src/atree_shared"
     ]
+    |> Enum.concat(config_action_paths())
     |> Enum.map(&Path.expand/1)
     |> cleanup_paths()
   end
@@ -14,11 +15,30 @@ defmodule Atree.Config do
       "~/.atree/playbooks"
     ]
     |> Enum.concat(action_privs())
+    |> Enum.concat(config_playbook_paths())
     |> List.flatten()
     |> cleanup_paths()
   end
 
   # ---------------------------------------
+
+  defp config_data do
+    file = Path.expand("~/.atree/config.yaml")
+    if File.exists?(file) do
+      file
+      |> YamlElixir.read_from_file!()
+    else
+      %{}
+    end
+  end
+
+  defp config_action_paths do
+    config_data()[:action_paths] || []
+  end
+
+  defp config_playbook_paths do
+    config_data()[:playbook_paths] || []
+  end
 
   defp cleanup_paths(list) do
     list
